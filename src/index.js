88a28c7ad4e93960f751d89f09934f98f61c80f2
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2021-04-10 16:24:55 +0200

    added link on homepage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
+const nocache = require('nocache');
 const app = express();
 const port = 80;
 
-const signalling = require('./signaling/signaling.js');
+const signaling = require('./signaling/signaling.js');
 
 app.disable('x-powered-by');
 
@@ -11,11 +12,14 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, "views"));
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
+app.use(nocache());
+app.disable('etag');
+
 const homepage = require(path.join(__dirname, 'routes', 'homepage.js'));
 
 
 app.use('/', homepage);
 
 app.listen(port, () => {
-    console.log(`miedzy-nami-web listening on port ${port}`);
+	console.log(`miedzy-nami-web listening on port ${port}`);
 })
