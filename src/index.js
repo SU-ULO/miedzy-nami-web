8effe09b2e885e392df5dde513575a288c8a49d4
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2021-04-11 21:21:54 +0200

    moved game to /game directory and added favicon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const nocache = require('nocache');
+const favicon = require('serve-favicon');
 const app = express();
 const port = 80;
 
@@ -8,9 +9,11 @@ const signaling = require(path.join(__dirname, 'signaling', 'signaling.js'));
 
 app.disable('x-powered-by');
 
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, "views"));
 app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/game', express.static(path.join(__dirname, 'game')));
 
 app.use(nocache());
 app.disable('etag');
